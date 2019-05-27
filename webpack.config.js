14956888110153083f17227850c9fbc5447868eb
kurtianyk <o.kurtianyk@gmail.com>
2019-05-27 10:42:30 +0300

    change structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,76 +3,79 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Autoprefixer = require('autoprefixer');
 
-const outputDirectory = 'dist';
-const devMode = process.env.NODE_ENV === 'development';
-
-const config = {
-  entry: './src/client/index.js',
-  output: {
-    path: `${__dirname}/${outputDirectory}`,
-    filename: '[hash].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { minimize: true } },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [Autoprefixer({
-                browsers: ['> 1%', 'last 2 versions']
-              })],
-            }
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        secure: false
-      }
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({ template: './src/client/index.html' }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    }),
-  ],
-};
-
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.devtool = 'source-map';
-  }
 
-  return config;
+  const outputDirectory = 'dist';
+  const devMode = process.env.NODE_ENV === 'development';
+
+  return  {
+    entry: './client/index.js',
+    output: {
+      path: `${__dirname}/${outputDirectory}`,
+      filename: '[hash].bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            { loader: 'css-loader', options: { minimize: true } },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [Autoprefixer({
+                  browsers: ['> 1%', 'last 2 versions']
+                })],
+              }
+            },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          loader: 'url-loader?limit=100000',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devtool: 'source-map',
+      watch: true,
+      node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+      },
+
+    plugins: [
+      new CleanWebpackPlugin([outputDirectory]),
+      new HtmlWebpackPlugin({ template: './src/client/index.html' }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+    ],
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:4000',
+          secure: false
+        }
+      }
+    },
+  };
 };
