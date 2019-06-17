3d642c92a33c510cead542fd05d7bc5a41005b21
kurtianyk <o.kurtianyk@gmail.com>
2019-06-17 12:36:43 +0300

    error handler

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -31,6 +32,19 @@ app.use(express.static('dist'));
 
 // app.use('/api', routes);
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, `../${isProduction ? 'dist' : 'client'}/index.html`)));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.sendStatus(err.status || 500);
+});
 
 mongoose.set('useCreateIndex', true);
 
